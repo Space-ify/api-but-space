10ea59d3caf8d4178501476e90497361813c7615
Jayden Pyles <jpylesbusiness@gmail.com>
2024-02-03 16:30:31 -0600

    wip: add in image generator

---
+++ src/api/generator/ImageGenerator.py
@@ -0,0 +1,14 @@
+# STL
+import math
+import random
+
+# PDM
+from PIL import Image, ImageDraw
+
+
+class ImageGenerator:
+    def __init__(self) -> None:
+        pass
+
+    def generate_image(self):
+        ...
