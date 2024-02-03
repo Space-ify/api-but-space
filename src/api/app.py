f56a4f5f225775bf8b238a7c268f276e708b61ad
Jayden Pyles <jpylesbusiness@gmail.com>
2024-02-03 17:58:15 -0600

    wip: add CORS

--- src/api/app.py
+++ src/api/app.py
@@ -4,6 +4,7 @@ import os
 # PDM
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # LOCAL
 from .routes.EdgeDBRoutes import edgedb_router
@@ -11,6 +12,16 @@ from .routes.SpotifyRoutes import spotify_router
 
 load_dotenv(".env")
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(edgedb_router)
 app.include_router(spotify_router)
